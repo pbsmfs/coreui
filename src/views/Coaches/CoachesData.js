de3db22fa70db41f67718aad69cdbd0cfb1276e3
twit <fsmart2006@gmail.com>
2020-04-30 18:16:26 +0300

    coaches now using their own data

--- src/views/Coaches/CoachesData.js
+++ src/views/Coaches/CoachesData.js
@@ -5,7 +5,6 @@ const coachesData = [
     {id: 3, name: 'AlienAlien', registered: '2018/02/01', role: 'Admin', status: 'Inactive'},
     {id: 4, name: 'AliAlienAlienen', registered: '2018/03/01', role: 'Member', status: 'Pending'},
     {id: 5, name: 'Alien', registered: '2018/01/21', role: 'Staff', status: 'Active'},
-    {id: 6, name: 'AlAlienien', registered: '2018/01/01', role: 'Member', status: 'Active'},
     {id: 7, name: 'Alien', registered: '2018/02/01', role: 'Staff', status: 'Banned'},
     {id: 8, name: 'AliAlienen', registered: '2018/02/01', role: 'Admin', status: 'Inactive'},
     {id: 9, name: 'AAlienlien', registered: '2018/03/01', role: 'Member', status: 'Pending'},
